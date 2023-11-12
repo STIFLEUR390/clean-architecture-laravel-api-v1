8142d0453a37293cf86e8083f6e20aa2d7538173
Dev Master <heroldtamko39@gmail.com>
2023-11-12 13:04:56 +0100

    add policies to auth provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,22 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use App\Models\Address;
 use App\Models\Category;
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\OrderPayment;
+use App\Models\Product;
+use App\Models\Transfer;
+use App\Policies\AddressPolicy;
 use App\Policies\CategoryPolicy;
+use App\Policies\CustomerPolicy;
+use App\Policies\OrderDetailPolicy;
+use App\Policies\OrderPaymentPolicy;
+use App\Policies\OrderPolicy;
+use App\Policies\ProductPolicy;
+use App\Policies\TransferPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -17,6 +31,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Category::class => CategoryPolicy::class,
+        Address::class => AddressPolicy::class,
+        Customer::class => CustomerPolicy::class,
+        Order::class => OrderPolicy::class,
+        OrderDetail::class => OrderDetailPolicy::class,
+        OrderPayment::class => OrderPaymentPolicy::class,
+        Product::class => ProductPolicy::class,
+        Transfer::class => TransferPolicy::class,
     ];
 
     /**
